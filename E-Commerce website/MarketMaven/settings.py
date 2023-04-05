a6bb1d2f63aa9c14b0765263980292a2cb3e12e1
Abhishek Mishra <79761277+AbhiMishra-Now@users.noreply.github.com>
2023-04-06 00:01:13 +0530

    Update settings.py

--- E-Commerce website/MarketMaven/settings.py
+++ E-Commerce website/MarketMaven/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'yiswasjd2_9cnigx09716838!j515xpzg50%fp@b*99c$nm59o'
+SECRET_KEY = ' '
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
